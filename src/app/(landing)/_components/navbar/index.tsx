3443c95cc5a0477a26523b66e5d9cc7e0b6520d3
chamaahandsome <timboywa@gmail.com>
2024-09-11 22:31:01 -0400

    Update landing page navbar and add logo image

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Menu from "./menu";
 
 type Props = {};
@@ -5,7 +6,16 @@ type Props = {};
 const LandingPageNavBar = (props: Props) => {
   return (
     <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
+      <div className="w-full flex">
+      <Image
+              src="/MaltivasityLogo.svg"
+              alt="Maltivasity Logo"
+              width={34}
+              height={34}
+              priority
+              />
       <p className="text-2xl font-bold">Maltivasity.</p>
+      </div>
       <Menu orientation="desktop" />
     </div>
   );
